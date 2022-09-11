e6ee317b1117c2fe19d4f3a73af05e3b7281fbde
Даниил Сыткин <90085209+danils2018@users.noreply.github.com>
2022-09-11 17:19:32 +0300

    Add files via upload

--- 1 lab 1 task.c
+++ 1 lab 1 task.c
@@ -7,8 +7,7 @@
 long long conv_n_to_int(char* s, int* check_error_in_n){
     int i, minus = 0, k = 0;
     long long n = 0;
-
-    for (i = 0; s[i] != '\0'; i++) {
+    for (i = 0; s[i] != '\0'; ++i) {
         if (i == 0) {
             if (s[i] == '-') {
                 minus = 1;
@@ -30,8 +29,6 @@ long long conv_n_to_int(char* s, int* check_error_in_n){
                 break;
             }
         }
-
-
     }
     if (k == 0){
         *check_error_in_n = -3;
@@ -42,16 +39,15 @@ long long conv_n_to_int(char* s, int* check_error_in_n){
     if (n <= 0) {
         *check_error_in_n = -4;
     }
-
     return n;
 } //Здесь коды ошибок кладутся в чек ерор и одновременно с этим переводится число из строки в ll
 
-int Prime(long long n){
+int prime(long long n){
     if (n < 2) return 0;
     long long i;
     int k = 1;
-    for (i = 2; i <= floor(sqrt(n)); i++) {
-        if(n % i == 0) {
+    for (i = 2; i <= floor(sqrt(n)); ++i) {
+        if(!(n % i)) {
             k = 0;
             break;
         }
@@ -62,7 +58,7 @@ int Prime(long long n){
 long long factorial(long long n){
     long long p = 1;
     if (n > 1) {
-        for (int i = 2; i <= n; i ++){
+        for (int i = 2; i <= n; ++i){
             if(LLONG_MAX / p >= i) {
                 p *= i;
             } else {
@@ -71,7 +67,9 @@ long long factorial(long long n){
         }
         return p;
     }
-    else return 1;
+    else {
+        return 1;
+    }
 } //Факториал
 
 int entered_parameters_verify(long long n, int argc, char* argv, int check_error_in_n) {
@@ -92,8 +90,8 @@ int entered_parameters_verify(long long n, int argc, char* argv, int check_error
         return 1;
     }
     int i, k = 0;
-    for (i = 0; argv[i] != '\0'; i++) {
-        k ++;
+    for (i = 0; argv[i] != '\0'; ++i) {
+        k++;
     }
     if (k != 2) {
         printf("Flag entered incorrectly! (must be length 2)\n");
@@ -105,11 +103,11 @@ int entered_parameters_verify(long long n, int argc, char* argv, int check_error
 int main(int argc, char *argv[]) {
     int check_error_in_n = 1;
     long long power_func_e;
-    int i, l, i1, j1, x, ih, kh, good_flag = 0, ERRORS = 1;
+    int i, l, i1, j1, x, ih, kh;
+    int good_flag = 0, ERRORS = 1;
     long long n;
     if (argc > 1) {
         n = conv_n_to_int(argv[1], &check_error_in_n);
-        //printf("%d\n", check_error_in_n);
         ERRORS = entered_parameters_verify(n, argc, argv[2], check_error_in_n);
     } else {
         printf("Parameters no entered!\n");
@@ -122,13 +120,13 @@ int main(int argc, char *argv[]) {
 
 
     for (i = 0; i < 12; i += 2) {
-        if (strcmp(parametrs[i], argv[2]) == 0 || strcmp(parametrs[i + 1], argv[2]) == 0) {
+        if (!strcmp(parametrs[i], argv[2]) || !strcmp(parametrs[i + 1], argv[2])) {
             good_flag = 1;
             switch (parametrs[i][1]) {
                 case 'h':
                     kh = 0;
                     for (ih = 0; ih <= 100 ; ++ih) {
-                        if (ih % n == 0) {
+                        if (!(ih % n)) {
                             printf("%d ", ih);
                             kh++;
                         }
@@ -141,7 +139,7 @@ int main(int argc, char *argv[]) {
                     printf("\n");
                     break;
                 case 'p':
-                    x = Prime(n);
+                    x = prime(n);
                     if(x) {
                         printf("The number %lld is prime!\n", n);
                     } else{
@@ -149,7 +147,7 @@ int main(int argc, char *argv[]) {
                     }
                     break;
                 case 's':
-                    for(l = 0; argv[1][l] != '\0'; l++)
+                    for(l = 0; argv[1][l] != '\0'; ++l)
                     {
                         printf("%c ", argv[1][l]);
                     }
@@ -157,10 +155,10 @@ int main(int argc, char *argv[]) {
                     break;
                 case 'e':
                     if(n <= 10) {
-                        for (i1 = 1; i1 <= 10; i1++) {
+                        for (i1 = 1; i1 <= 10; ++i1) {
                             printf("%d - ", i1);
                             power_func_e = i1;
-                            for (j1 = 1; j1 <= n; j1++) {
+                            for (j1 = 1; j1 <= n; ++j1) {
                                 printf("%lld ", power_func_e);
                                 power_func_e *= i1;
                             }
