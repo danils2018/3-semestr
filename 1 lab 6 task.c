49d74aa10cbece76180a0f1187fba8de86a85b0f
Даниил Сыткин <90085209+danils2018@users.noreply.github.com>
2022-09-15 00:35:42 +0300

    Update 1 lab 6 task.c

--- 1 lab 6 task.c
+++ 1 lab 6 task.c
@@ -91,8 +91,8 @@ int main(int argc, char *argv[]) {
             flag = 1;
             max = 0;
             i = 0;
-            if (c == EOF) break;
         }
+        if (c == EOF) break;
     }
 
     fclose(input_file);
