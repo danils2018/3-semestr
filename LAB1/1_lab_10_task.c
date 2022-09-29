a605eacd56564db7343dbb784424267b7c17a618
Даниил Сыткин <90085209+danils2018@users.noreply.github.com>
2022-09-29 16:52:10 +0300

    Add files via upload

--- LAB1/1_lab_10_task.c
+++ LAB1/1_lab_10_task.c
@@ -1,211 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
 #include <time.h>
 
-int ** matr(int ** a, int n, int x){
-    int ** res = (int **)malloc((n - 1) * sizeof(int *));
-    for (int i = 0; i < n - 1; ++i)
-        res[i] = (int *)malloc((n - 1) * sizeof(int));
-    for (int i = 1; i < n; ++i)
-        for (int j = 0, k = 0; j < n; ++j, ++k){
-            if (j == x){
-                --k;
-                continue;
-            }
-            res[i - 1][k] = a[i][j];
-        }
-    return res;
-}
+#define ERROR 0.01
 
-long long determinant(int ** a, int n){
-    if (n == 1)
-        return a[0][0];
-    if (n == 2)
-        return (a[1][1] * a[0][0]) - (a[0][1] * a[1][0]);
-    long long ans = 0, sig = 1;
-    for (int i = 0; i < n; ++i){
-        ans += (sig * a[0][i] * determinant(matr(a, n, i),n - 1));
-        sig *= -1;
+void free_matrix(double **matrix, int row) {
+    for (int i = 0; i < row; i++) {
+        free(matrix[i]);
     }
-    return ans;
+    free(matrix);
+}
+
+double **generate_matrix(int *row, int *col) {
+    //*row = rand() % 10 + 1;
+    //*col = rand() % 10 + 1;
+
+    double **matrix = (double**)malloc(sizeof(double*) * *row);
+    for (int i = 0; i < *row; i++) {
+        matrix[i] = (double*)malloc(sizeof(double) * *col);
+        for (int j = 0; j < *col; j++) {
+            matrix[i][j] = rand() % 200 - 100;
+        }
+    }
+
+    return matrix;
+}
+
+void output_matrix(double **matrix, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%10.0lf ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+double **multiply_matrix(double **matrix_1, int row_1, int col_1, double **matrix_2, int row_2, int col_2) {
+    double **result = NULL;
+
+    if (col_1 == row_2) {
+        result = (double**)malloc(sizeof(double*) * row_1);
+        for (int i = 0; i < row_1; i++) {
+            result[i] = (double*)malloc(sizeof(double) * col_2);
+        }
+
+        for (int i = 0; i < row_1; i++) {
+            for (int j = 0; j < col_2; j++) {
+                result[i][j] = 0;
+                for (int k = 0; k < col_1; k++) {
+                    result[i][j] += matrix_1[i][k] * matrix_2[k][j];
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+void row_decrement(double *row_begin, double *row, double dec, int col) {
+    for (int i = 0; i < col; i++) {
+        row[i] -= dec * row_begin[i];
+    }
 }
 
 
-int main(int argc, char *argv[]) {
+double determinant(double **matrix, int row, int col) {
+    if (row == col) {
+        double res = 1.0;
+        int size = row;
+        double **copy_matrix = (double**)malloc(sizeof(double*) * size);
+
+        for (int i = 0; i < size; i++) {
+            copy_matrix[i] = (double*)malloc(sizeof(double) * size);
+            for (int j = 0; j < size; j++) {
+                copy_matrix[i][j] = matrix[i][j];
+            }
+        }
+
+        for (int row_begin = 0; row_begin < size; row_begin++) {
+            for (int row_dec = row_begin + 1; row_dec < size; row_dec++) {
+                row_decrement(copy_matrix[row_begin], copy_matrix[row_dec], copy_matrix[row_dec][row_begin] / copy_matrix[row_begin][row_begin], size);
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            res *= copy_matrix[i][i];
+        }
+
+        free_matrix(copy_matrix, size);
+
+        return res;
+    }
+
+    return ERROR;
+}
+
+
+
+
+int main() {
     srand(time(0));
-    int n = 1 + rand() % 10, m = 1 + rand() % 10;
-    int n2 = 1 + rand() % 10, m2 = 1 + rand() % 10;
-    //n = 10; m = 10; n2 = 10; m2 = 10;
-    int i, j, k, z;
+    int row_1 = 3;
+    int col_1 = 3;
+    int row_2 = 3;
+    int col_2 = 3;
+    double **matrix_1 = generate_matrix(&row_1, &col_1);
+    double **matrix_2 = generate_matrix(&row_2, &col_2);
+    double **multiply = multiply_matrix(matrix_1, row_1, col_1, matrix_2, row_2, col_2);
 
-    char parametrs [3][3] = {"a)", "b)", "c)"};
+    printf("A:\n");
+    output_matrix(matrix_1, row_1, col_1);
+    printf("B:\n");
+    output_matrix(matrix_2, row_2, col_2);
 
-    int good_flag = 0;
-    for (k = 0; k < 5; k++) {
-        if (!strcmp(parametrs[k], argv[1])) {
-            good_flag = 1;
-            switch (parametrs[k][0]) {
-                case 'a':
-                    printf("\nMatrix:\n");
-                    int** matrix = (int**) malloc(sizeof(int*) * n);
-                    for(i = 0; i < n; i ++)
-                    {
-                        matrix[i] = (int*) malloc(sizeof(int) * m);
-                        for (j = 0; j < m; j++)
-                        {
-                            matrix[i][j] = rand() % 101 * (rand() % 2 == 0 ? -1 : 1);
-                            printf("%d ", matrix[i][j]);
-                        }
-                        printf("\n");
-                    }
+    if (multiply != NULL) {
+        printf("A x B:\n");
+        output_matrix(multiply, row_1, col_2);
+    } else {
+        fprintf(stderr, "%s\n\n", "A x B:\nMultiplication cannot be done");
+    }
 
-                    for(i = 0; i < n; i ++)
-                    {
-                        free(matrix[i]);
-                    }
+    if (determinant(matrix_1, row_1, col_1) != ERROR) {
+        printf("det(A) = %lf\n", determinant(matrix_1, row_1, col_1));
+    } else {
+        fprintf(stderr, "%s\n\n", "det(A): Invalid count of row and col");
+    }
 
-                    free(matrix);
+    if (determinant(matrix_2, row_2, col_2) != ERROR) {
+        printf("det(B) = %lf\n", determinant(matrix_2, row_2, col_2));
+    } else {
+        fprintf(stderr, "%s\n\n", "det(B): Invalid count of row and col");
+    }
 
-                    break;
-                case 'b':
-                    printf("\nMatrix 1:\n");
-                    int** matrix1 = (int**) malloc(sizeof(int*) * n);
-
-                    for(i = 0; i < n; i ++)
-                    {
-                        matrix1[i] = (int*) malloc(sizeof(int) * m);
-                        for (j = 0; j < m; j++)
-                        {
-                            matrix1[i][j] = rand() % 101 * (rand() % 2 == 0 ? -1 : 1);
-                            printf("%d ", matrix1[i][j]);
-                        }
-                        printf("\n");
-                    }
-
-                    printf("\nMatrix 2:\n");
-                    int** matrix2 = (int**) malloc(sizeof(int*) * n2);
-                    for(i = 0; i < n2; i ++)
-                    {
-                        matrix2[i] = (int*) malloc(sizeof(int) * m2);
-                        for (j = 0; j < m2; j++)
-                        {
-                            matrix2[i][j] = rand() % 101 * (rand() % 2 == 0 ? -1 : 1);
-                            printf("%d ", matrix2[i][j]);
-                        }
-                        printf("\n");
-                    }
-                    printf("\n");
-                    if(m != n2) {
-                        printf("Unfortunately, we can`t multiply these matrices!\n");
-                        return 0;
-                    }
-
-                    int** matrix3 = (int**) malloc(sizeof(int*) * n);
-                    for(i = 0; i < n; i ++)
-                    {
-                        matrix3[i] = (int*) malloc(sizeof(int) * m2);
-                        for (j = 0; j < m2; j++)
-                        {
-                            matrix3[i][j] = 0;
-                        }
-                    }
-
-                    for(i = 0; i < n; i++)
-                    {
-                        for(j = 0; j < m2; j++)
-                        {
-                            for(z = 0; z < m; z++)
-                                matrix3[i][j] += matrix1[i][z] * matrix2[z][j];
-                        }
-                    }
-
-                    printf("Result matrix:\n");
-
-                    for(i = 0; i < n; i ++)
-                    {
-                        for (j = 0; j < m2; j++)
-                        {
-                            printf("%d ", matrix3[i][j]);
-                        }
-                        printf("\n");
-                    }
-
-
-
-
-
-
-                    for(i = 0; i < m; i ++)
-                    {
-                        free(matrix1[i]);
-                    }
-                    free(matrix1);
-                    for(i = 0; i < m2; i ++)
-                    {
-                        free(matrix2[i]);
-                        free(matrix3[i]);
-                    }
-                    free(matrix2);
-                    free(matrix3);
-                    break;
-                case 'c':
-                    printf("\nMatrix 1:\n");
-                    int** Matrix1 = (int**) malloc(sizeof(int*) * n);
-
-                    for(i = 0; i < n; i ++)
-                    {
-                        Matrix1[i] = (int*) malloc(sizeof(int) * m);
-                        for (j = 0; j < m; j++)
-                        {
-                            Matrix1[i][j] = rand() % 101 * (rand() % 2 == 0 ? -1 : 1);
-                            printf("%d ", Matrix1[i][j]);
-                        }
-                        printf("\n");
-                    }
-                    if (n != m) {
-                        printf("\nUnable to calculate determinant!\n");
-                    } else {
-                        printf("\nDeterminant: %lld\n",determinant(Matrix1, n));
-                    }
-
-                    printf("\nMatrix 2:\n");
-                    int** Matrix2 = (int**) malloc(sizeof(int*) * n2);
-                    for(i = 0; i < n2; i ++)
-                    {
-                        Matrix2[i] = (int*) malloc(sizeof(int) * m2);
-                        for (j = 0; j < m2; j++)
-                        {
-                            Matrix2[i][j] = rand() % 101 * (rand() % 2 == 0 ? -1 : 1);
-                            printf("%d ", Matrix2[i][j]);
-                        }
-                        printf("\n");
-                    }
-                    if (n2 != m2) {
-                        printf("\nUnable to calculate determinant!\n");
-                    } else {
-                        printf("\nDeterminant: %lld\n",determinant(Matrix2, n2));
-                    }
-
-                    for(i = 0; i < m; i ++)
-                    {
-                        free(Matrix1[i]);
-                    }
-                    free(Matrix1);
-                    for(i = 0; i < m2; i ++)
-                    {
-                        free(Matrix2[i]);
-                    }
-                    free(Matrix2);
-
-                    break;
-            }
+    if (multiply != NULL) {
+        if (determinant(multiply, row_1, col_2) != ERROR) {
+            printf("det(A x B) = %lf\n", determinant(multiply, row_1, col_2));
+        } else {
+            fprintf(stderr, "%s\n\n", "det(A x B): Invalid count of row and col");
         }
+        free_matrix(multiply, row_1);
+    } else {
+        fprintf(stderr, "%s\n\n", "det(A x B): Multiplication cannot be done");
     }
-    if(!good_flag) {
-        printf("Invalid flag entered! flag one of the ( 'a)' , 'b)', 'c)', 'd)', 'e)' )\n");
-        return 0;
-    }
+
+    free_matrix(matrix_1, row_1);
+    free_matrix(matrix_2, row_2);
+
     return 0;
 }
+
+
+
+
+
